1187818bdd6abe9b9a9200b37a53901e576acbf6
Vitalii Yakubovskyi <123841115+Vitalii2594@users.noreply.github.com>
2024-08-28 11:17:56 +0200

    CAR script.js

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
-function handleSubmit(event) {
-    event.preventDefault();
-    
-    const form = event.target;
-    const formData = new FormData(form);
+// JavaScript for handling pop-up display
+document.getElementById('cta-button').addEventListener('click', function() {
+    document.getElementById('order-form-popup').style.display = 'flex';
+});
 
-    // Collect data from form fields
-    const name = formData.get('name');
-    const email = formData.get('email');
-    const phone = formData.get('phone');
-    const contactByEmail = formData.get('contactByEmail');
-    const contactByPhone = formData.get('contactByPhone');
-    const topics = formData.getAll('topics');
+document.querySelector('.close-button').addEventListener('click', function() {
+    document.getElementById('order-form-popup').style.display = 'none';
+});
 
-    // Determine contact preference
-    let contactMethod = '';
-    if (contactByEmail) {
-        contactMethod += ' email';
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('order-form-popup')) {
+        document.getElementById('order-form-popup').style.display = 'none';
     }
-    if (contactByPhone) {
-        contactMethod += ' phone';
-    }
-
-    // Prepare email content
-    const mailBody = `Dane osoby zainteresowanej kontaktem z DEKRA:
-    Imię i nazwisko: ${name}
-    E-mail: ${email}
-    Telefon: ${phone}
-    Preferowana forma kontaktu:${contactMethod}
-    Wybrane tematy: ${topics.join(', ')}`;
-
-    const mailtoLink = `mailto:vitalii.yakubovskyi@dekra.com?subject=Wynik ankiety&body=${encodeURIComponent(mailBody)}`;
-    window.location.href = mailtoLink;
-
-    alert('Dziękujemy za wypełnienie formularza! Zostaniesz przekierowany do aplikacji pocztowej, aby wysłać wiadomość.');
-}
+});
